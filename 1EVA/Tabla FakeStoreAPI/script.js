1b61ef7db48511831300a499fcb7af3a0c894668
AlvarocsED <acastellanos02@educarex.es>
2023-12-04 12:33:38 +0100

    11

---
+++ 1EVA/Tabla FakeStoreAPI/script.js
@@ -0,0 +1,18 @@
+window.onload=inicio;
+
+function inicio(){
+ let btn=document.getElementById("listar1");
+ btn.onclick=function(){
+   fetch('https://fakestoreapi.com/products')
+   .then(response => response.json())
+  .then(datos =>{
+    console.table(datos);
+    
+    cargarTabla(datos);
+  } );
+ }
+}
+function cargarTabla(datos){
+   console.log("Muestro la tabla");
+   
+}
