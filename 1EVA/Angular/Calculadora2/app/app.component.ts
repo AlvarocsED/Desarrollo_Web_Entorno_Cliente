dec9154bbecb70b515a413313233896e80092038
AlvarocsED <acastellanos02@educarex.es>
2023-12-18 13:28:31 +0100

    calculadoraAngular

---
+++ 1EVA/Angular/Calculadora2/app/app.component.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ visor:string="";
+ operador:number=0;
+ guardar:number=0;
+ arrastra:boolean=false;
+   operar(valor:string){
+     if (Number(valor)>=0){
+          this.visor+=valor;
+     }else
+     {
+       switch(valor){
+        case "-1":{
+          //dividir
+          if (this.arrastra) {
+            if (Number(this.visor) !==0) {
+              this.visor=String(this.guardar/Number (this.visor));
+            }else{
+              this.visor="Error";
+            }
+            this.guardar=0;
+
+          }
+          this.arrastra=false;
+          this.operador=-1
+          break;
+        }
+        case "-2":{
+          //multiplicar
+          break;
+        }
+        case "-3":{
+          //sumar
+          if (this.arrastra==true){
+            this.visor=String(Number(this.visor)+this.guardar);
+            //versión de acumular más de 2 operandos
+            this.guardar=Number(this.visor);
+            //después de cada número reseteo el visor para que no concatenea
+            this.visor="";
+
+          }else
+          {
+            this.guardar=Number(this.visor);
+            this.visor="";
+            this.arrastra=true;
+          }
+          this.operador=-3;
+          break;
+        }
+        case "-4":{
+          //decimal
+          break;
+        }
+
+        case "-5":{
+          //igual
+          console.log(this.visor);
+          if (this.operador==-3){
+            this.visor=String(Number(this.visor)+this.guardar);
+          }
+          break;
+        }
+        case "-6":{
+          //restrar
+          break;
+        }
+        case "-7":{
+          //reseteo total
+          this.visor="";
+          this.operador=0;
+          this.guardar=0;
+          this.arrastra=false;
+          break;
+        }
+        case "-8":{
+          //eliminar el visor
+          break;
+        }
+
+       }
+
+     }
+   }
+  }
+
+
