367ceb4f793f46d5c09886e85bd7d7ac8987276e
AlvarocsED <acastellanos02@educarex.es>
2024-01-08 13:17:31 +0100

    huerto

---
+++ 2EVA/Angular/Huerto/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HortalizaComponent } from './hortaliza/hortaliza.component';
+import { FrutalComponent } from './frutal/frutal.component';
+import { AromaticasComponent } from './aromaticas/aromaticas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HortalizaComponent,
+    FrutalComponent,
+    AromaticasComponent,
+    AromaticasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
