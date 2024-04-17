a63919734d807a523048b8f3e7ec647ae86e1886
AlvarocsED <acastellanos02@educarex.es>
2024-04-17 13:30:55 +0200

    a

---
+++ Repaso Marzo-Junio/17 abril videos/js/script.js
@@ -0,0 +1,31 @@
+window.addEventListener("load",inicio);
+
+function inicio(){
+        
+       
+           let btnMostrar= document.getElementById("mostrar");
+           btnMostrar.addEventListener("click", mostrar);
+        }
+
+        function mostrar() {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = cargar;
+            function cargar() {
+                if (this.readyState == 4 && this.status == 200) {
+                    //Al hacer parse nos devuelve un objeto
+                    var objeto = JSON.parse(this.responseText);
+
+                   
+                   objeto.forEach(recorrer);
+                   function recorrer(item,index){
+
+                    document.getElementById("parrafo").innerHTML += "<video src='"+item.url +"' width='900' height='700' autoplay loop></video>";   
+                    document.getElementById("parrafo").className="gallery";
+                   }
+                
+            }
+             }
+            xhr.open("GET", "http://camacho.atwebpages.com/webcam/getWebcam.php", true);
+            xhr.send();
+       
+            }
