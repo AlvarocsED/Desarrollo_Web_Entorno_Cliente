7188ad8b42125e7fda4b8552fab5c9374b9ee5b7
AlvarocsED <acastellanos02@educarex.es>
2024-04-22 19:37:37 +0200

    1

--- Repaso Marzo-Junio/Practica2- 19 Abril/script.js
+++ Repaso Marzo-Junio/Practica2- 19 Abril/script.js
@@ -1,57 +1,23 @@
 window.addEventListener("load", inicio);
+var idCajaTabla=document.querySelector("#cajaTabla");
 function inicio(){
-
- function obtenerDatos() {
-    try {
-      const response =  fetch("http://moralo.atwebpages.com/menuAjax/Provincias/getProvincias.php");
-      const datos =  response.json();
-      return datos;
-    } catch (error) {
-      console.error("Error al obtener los datos:", error);
-      return [];
-    }
-  }
-  
-
-   function determinarGanadores() {
-    const datos =  obtenerDatos();
-  
-    datos.forEach(provincia => {
-      const partidoGanador = provincia.partido_ganador;
-      if (partidoGanador) {
-        datosPartidos[partidoGanador].provinciasGanadas.push(provincia.nombre);
+  fetch('http://moralo.atwebpages.com/menuAjax/Provincias/getProvincias.php')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
       }
-    });
-  
-    
-    mostrarResultadosEnHTML();
-  }
-  
-  
-  function mostrarResultadosEnHTML() {
-    const representantesDiv = document.getElementById("representantes");
-    const provinciasDiv = document.getElementById("provincias");
-    const votosDiv = document.getElementById("votos");
-    const ordenDiv = document.getElementById("orden");
-  
-    representantesDiv.innerHTML = "";
-    provinciasDiv.innerHTML = "";
-    votosDiv.innerHTML = "";
-    ordenDiv.innerHTML = "";
-  
-    for (const partido in datosPartidos) {
-      representantesDiv.innerHTML += `<p>${partido}: ${datosPartidos[partido].provinciasGanadas.length} representantes</p>`;
-      provinciasDiv.innerHTML += `<p>${partido}: ${datosPartidos[partido].provinciasGanadas.join(', ')}</p>`;
-      votosDiv.innerHTML += `<p>${partido}: ${datosPartidos[partido].votos} votos</p>`;
-    }
-  
-    const partidosOrdenados = Object.keys(datosPartidos).sort((a, b) => datosPartidos[b].votos - datosPartidos[a].votos);
-    partidosOrdenados.forEach((partido, index) => {
-      ordenDiv.innerHTML += `<p>${index + 1}. ${partido}</p>`;
-    });
-  }
-  
-  
-  determinarGanadores();
-  
+      return response.json();
+  })
+  .then(data => {
+      console.log('Contenido JSON:', data);
+      data.forEach(provincia => {
+        idCajaTabla.innerHTML+=provincia.nombreProvincia+"-"+
+        provincia.votosPA+"<br>";
+      });
+  })
+  .catch(error => {
+      console.error('Hubo un problema con la solicitud fetch:', error);
+  });
+
+
 }
